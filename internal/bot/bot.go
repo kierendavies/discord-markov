4346a6475c3b4c8995210b995ad2aaacd40956dc
Kieren Davies <kieren@kdavi.es>
2020-03-21 12:32:45 +0100

    Run DB garbage collection periodically

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"math"
 	"math/rand"
+	"sync"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/dgraph-io/badger/v2"
@@ -12,11 +14,17 @@ import (
 // 1 in 1000
 const respProbThreshold = math.MaxUint64 / 1000
 
+const dbGCInterval = 5 * time.Minute
+const dbGCDiscardRatio = 0.5
+
 // Bot represents a Markov bot with a Discord session and a database.
 type Bot struct {
-	session *discordgo.Session
-	dbOpts  badger.Options
-	db      *badger.DB
+	session    *discordgo.Session
+	dbOpts     badger.Options
+	db         *badger.DB
+	dbGCTicker *time.Ticker
+	stopCh     chan struct{}
+	waitGroup  *sync.WaitGroup
 }
 
 // New creates and configures a Bot.
@@ -28,7 +36,11 @@ func New(token string, dbPath string) (*Bot, error) {
 		return nil, err
 	}
 
-	b := &Bot{s, dbOpts, nil}
+	b := &Bot{
+		session:   s,
+		dbOpts:    dbOpts,
+		waitGroup: &sync.WaitGroup{},
+	}
 
 	s.AddHandler(func(s2 *discordgo.Session, m *discordgo.MessageCreate) {
 		if s2 != b.session {
@@ -126,12 +138,29 @@ func (b *Bot) handleMessageCreate(m *discordgo.MessageCreate) {
 
 // Open opens the database and creates a websocket connection to Discord.
 func (b *Bot) Open() error {
+	b.stopCh = make(chan struct{})
+
 	db, err := badger.Open(b.dbOpts)
 	if err != nil {
 		b.session.Close()
 		return err
 	}
 	b.db = db
+
+	b.dbGCTicker = time.NewTicker(dbGCInterval)
+	b.waitGroup.Add(1)
+	go func() {
+		defer b.waitGroup.Done()
+		for {
+			select {
+			case <-b.dbGCTicker.C:
+				log.Print("Running database garbage collection")
+				b.db.RunValueLogGC(dbGCDiscardRatio)
+			case <-b.stopCh:
+				return
+			}
+		}
+	}()
 
 	err = b.session.Open()
 	if err != nil {
@@ -143,6 +172,10 @@ func (b *Bot) Open() error {
 
 // Close closes the database and the websocket connection to Discord.
 func (b *Bot) Close() error {
+	b.dbGCTicker.Stop()
+	close(b.stopCh)
+	b.waitGroup.Wait()
+
 	sErr := b.session.Close()
 	dbErr := b.db.Close()
 	// The dbErr is more important so return that one if it exists.
